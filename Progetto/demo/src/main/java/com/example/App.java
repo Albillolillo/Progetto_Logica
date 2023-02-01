d936e8ce4928bf68f6dc5c9b095072e10ede08a1
Albillolillo <carpineto.alberto@gmail.com>
2023-02-01 15:36:43 +0100

    cazzo

--- Progetto/demo/src/main/java/com/example/App.java
+++ Progetto/demo/src/main/java/com/example/App.java
@@ -1,13 +1,14 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.logicng.formulas.*;
+
+public class App{
+    public static void main( String[] args ){
+        FormulaFactory f = new FormulaFactory();
+        Variable a = f.variable("A");
+        Variable b = f.variable("B");
+        Literal notC = f.literal("C", false);
+        Formula formula = f.and(a, f.not(f.or(b, notC)));
+        System.out.println( formula.toString());
     }
 }
